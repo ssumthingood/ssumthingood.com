57287a99edbc5d1f1ae296aa4752deeebad51522
ssumthingood <ssumthingood@gmail.com>
2021-01-25 01:52:55 +0900

    0125

--- src/Routes/Whoru/WhoruPresenter.js
+++ src/Routes/Whoru/WhoruPresenter.js
@@ -1,6 +1,24 @@
 import React from "react";
 import Wrapper from "Components/Wrapper";
 import Helmet from "react-helmet";
+import styled from "styled-components";
+
+const Main = styled.div`
+    padding-top:20px;
+    text-align:center;
+    font-size:50px;
+`;
+
+const Image = styled.div`
+    background-image:url(${props=>props.bgUrl});
+    margin:25px;
+    border : 3px solid black;
+    height:300px;
+    width:300px;
+    background-size:cover;
+    border-radius:150px;
+    background-position:center center;
+`;
 
 const Whoru = ()=>(
     <>
@@ -8,7 +26,8 @@ const Whoru = ()=>(
         <title>썸띵굿닷컴 | Whoru</title>
     </Helmet>
     <Wrapper>
-    <h1>Whoru</h1>
+    <Main>그래서 네가 누군데?</Main>
+    <Image bgUrl="ssumthingoodthumb1.png"/>
     </Wrapper>
     </>
 );
